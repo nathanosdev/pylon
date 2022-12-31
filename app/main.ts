d992ee4b7ad85b6915e3ac2bf5d7b857b4fdaac8
misscreeptoe <misscreeptoe@pm.me>
2022-12-31 20:33:29 +0100

    feat: add empty ICP protocol registration

--- app/main.ts
+++ app/main.ts
@@ -1,16 +1,14 @@
-import { app, BrowserWindow, screen } from 'electron';
+import { app, BrowserWindow, screen, protocol } from 'electron';
 import * as path from 'path';
 import * as fs from 'fs';
 
-let win: BrowserWindow = null;
-const args = process.argv.slice(1),
-  serve = args.some((val) => val === '--serve');
+const args = process.argv.slice(1);
+const serve = args.some((val) => val === '--serve');
 
-function createWindow(): BrowserWindow {
+function createWindow(): void {
   const size = screen.getPrimaryDisplay().workAreaSize;
 
-  // Create the browser window.
-  win = new BrowserWindow({
+  let browserWindow = new BrowserWindow({
     x: 0,
     y: 0,
     width: size.width,
@@ -27,7 +25,7 @@ function createWindow(): BrowserWindow {
     debug();
 
     require('electron-reloader')(module);
-    win.loadURL('http://localhost:4200');
+    browserWindow.loadURL('http://localhost:4200');
   } else {
     // Path when running electron executable
     let pathIndex = './index.html';
@@ -38,26 +36,78 @@ function createWindow(): BrowserWindow {
     }
 
     const url = new URL(path.join('file:', __dirname, pathIndex));
-    win.loadURL(url.href);
+    browserWindow.loadURL(url.href);
   }
 
+  browserWindow.webContents.session.webRequest.onBeforeRequest(
+    {
+      urls: ['http://*/*', 'https://*/*'],
+    },
+    (_details, callback) => {
+      // [TODO] - detect URLs meant for the IC
+
+      // replace URL protocol
+      // const redirectURL = details.url.replace(/^https?:\/\//, 'icp:');
+
+      // redirect to the ICP protocol
+      // callback({
+      //   redirectURL,
+      // });
+
+      // allow non-IC requests through as normal
+      callback({});
+    },
+  );
+
   // Emitted when the window is closed.
-  win.on('closed', () => {
+  browserWindow.on('closed', () => {
     // Dereference the window object, usually you would store window
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    win = null;
+    browserWindow = null;
   });
-
-  return win;
 }
 
 try {
+  protocol.registerSchemesAsPrivileged([
+    {
+      scheme: 'icp',
+      privileges: {
+        standard: true,
+        secure: true,
+        allowServiceWorkers: true,
+        supportFetchAPI: true,
+        corsEnabled: true,
+        stream: true,
+        bypassCSP: true,
+      },
+    },
+  ]);
+
   // This method will be called when Electron has finished
   // initialization and is ready to create browser windows.
   // Some APIs can only be used after this event occurs.
-  // Added 400 ms to fix the black background issue while using transparent window. More detais at https://github.com/electron/electron/issues/15947
-  app.on('ready', () => setTimeout(createWindow, 400));
+  app.whenReady().then(() => {
+    protocol.registerStreamProtocol('icp', async (protocolRequest, respond) => {
+      // [TODO] - fetch requested asset
+      console.log('ICP request', protocolRequest);
+
+      // [TODO] - certify asset
+
+      return respond({});
+    });
+
+    // Added 400 ms to fix the black background issue while using transparent window. More detais at https://github.com/electron/electron/issues/15947
+    setTimeout(createWindow, 400);
+
+    app.on('activate', () => {
+      // On OS X it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+      }
+    });
+  });
 
   // Quit when all windows are closed.
   app.on('window-all-closed', () => {
@@ -65,14 +115,6 @@ try {
     // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
       app.quit();
-    }
-  });
-
-  app.on('activate', () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (win === null) {
-      createWindow();
     }
   });
 } catch (e) {
